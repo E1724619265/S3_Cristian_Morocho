48cf73c804de431b598ce29b75bab962998f99b8
master <master@SMLAB408PC16_23>
2022-12-13 19:26:23 -0500

    Agregar archivos de proyecto.

---
+++ S3_Cristian_Morocho/login.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3_Cristian_Morocho
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class login : ContentPage
+    {
+        public login()
+        {
+            InitializeComponent();
+        }
+        private void btniniciar_Clicked(object sender, EventArgs e)
+        {
+            string usuario = "cmorocho";
+            string password = "12345678";
+
+            if(usuario == txtusuario.Text && password == txtpassword.Text)
+            {
+                Navigation.PushAsync(new registro(usuario));
+            }else
+            {
+                DisplayAlert("ALERTA", "USUARIO O CONTRASEÑA INCORRECTOS", "CERRAR");
+            }
+        }
+    }
+}
---
+++ S3_Cristian_Morocho/registro.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace S3_Cristian_Morocho
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro(string usuario)
+        {
+            InitializeComponent();
+
+            lblusuario.Text = usuario;
+        }
+
+   
+    }
+}
